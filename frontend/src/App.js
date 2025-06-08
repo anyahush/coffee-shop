b381fd7272bf0aa6d7fe32c16fe32823c1c53090
Anya McDonald <anyahush@gmail.com>
2025-06-08 16:27:01 +0100

    Add router to app.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
-import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import ReorderPage from './pages/ReorderPage';
+import ImportPage from './pages/ImportPage';
+import SuppliesPage  from "./pages/SuppliesPage";
 
 function App() {
-  return (
-      <div className="container mt-5">
-        <h1 className="text-primary">Hello, Bootstrap</h1>
-        <button className="btn btn-success">Click me</button>
-      </div>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<SuppliesPage />} />
+                    <Route path="reorder" element={<ReorderPage />} />
+                    <Route path="import"  element={<ImportPage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
-
 
 export default App;
